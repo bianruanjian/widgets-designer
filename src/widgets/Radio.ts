8b81a3164871f613412cf0331a8b11103bf3a1e9
jfx <jiangfx@iri.cn>
2018-06-13 13:44:35 +0800

    Checkbox, Link, Radio add overlay container

--- src/widgets/Radio.ts
+++ src/widgets/Radio.ts
@@ -1,3 +1,7 @@
 import DesignerWidgetMixin from 'widget-core-designer/DesignerWidgetMixin';
 import { RadioBase } from 'widgets-web/radio';
-export default class Radio extends DesignerWidgetMixin(RadioBase) {}
+export default class Radio extends DesignerWidgetMixin(RadioBase) {
+	protected needOverlay() {
+		return true;
+	}
+}
