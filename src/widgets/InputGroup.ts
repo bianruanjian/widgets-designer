c015bdd5d23bdd5482430e026e5ac8618068af76
jfx <jiangfx@iri.cn>
2018-06-14 09:58:20 +0800

    rename reOrder function

--- src/widgets/InputGroup.ts
+++ src/widgets/InputGroup.ts
@@ -6,7 +6,7 @@ export default class InputGroup extends DesignerWidgetMixin(InputGroupBase) {
 		return true;
 	}
 
-	protected reOrder() {
+	protected reOrderChildren() {
 		// 属性 position 需要结合子部件的位置来实现效果，故在此由程序根据 position 的值来自动调整子部件的位置
 		// 设计器版因为先渲染父部件，子部件此时还是 WNode 形态，故需要复写此方法
 		const prependChildren: VNode[] = [];
