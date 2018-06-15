05ba1a1714abeb74b324787fbe8a9206a92a968c
jfx <jiangfx@iri.cn>
2018-06-15 09:47:08 +0800

    resolve code review problem

--- src/widgets/InputGroup.ts
+++ src/widgets/InputGroup.ts
@@ -10,24 +10,25 @@ export default class InputGroup extends DesignerWidgetMixin(InputGroupBase) {
 		// 属性 position 需要结合子部件的位置来实现效果，故在此由程序根据 position 的值来自动调整子部件的位置
 		// 设计器版因为先渲染父部件，子部件此时还是 WNode 形态，故需要复写此方法
 		const prependChildren: VNode[] = [];
-		const otherChildren: VNode[] = [];
+		const inputChildren: VNode[] = [];
 		const appendChildren: VNode[] = [];
 		this.children.forEach((child, index) => {
 			if (child) {
-				const childName = (child as VNode).properties.widget.widgetName;
-				const position = (child as VNode).properties.widget.properties.position;
+				const childNode = child as VNode;
+				const childName = childNode.properties.widget.widgetName;
+				const position = childNode.properties.widget.properties.position;
 
 				if (childName === 'Addon') {
 					if (position && position === 'append') {
-						appendChildren.push(child as VNode);
+						appendChildren.push(childNode);
 					} else {
-						prependChildren.push(child as VNode);
+						prependChildren.push(childNode);
 					}
 				} else {
-					otherChildren.push(child as VNode);
+					inputChildren.push(childNode);
 				}
 			}
 		});
-		return [...prependChildren, ...otherChildren, ...appendChildren];
+		return [...prependChildren, ...inputChildren, ...appendChildren];
 	}
 }
