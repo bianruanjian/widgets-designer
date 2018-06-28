7cc066fa3027cb58f190e9f3441c40711693a2ed
jfx <jiangfx@iri.cn>
2018-06-28 20:17:18 +0800

    override getTagNameByChildNode function of ListGroup widget

--- src/widgets/ListGroup.ts
+++ src/widgets/ListGroup.ts
@@ -1,7 +1,35 @@
 import DesignerWidgetMixin from 'widget-core-designer/DesignerWidgetMixin';
 import { ListGroupBase } from 'widgets-web/list-group';
+import { VNode } from '@dojo/widget-core/interfaces';
 export default class ListGroup extends DesignerWidgetMixin(ListGroupBase) {
 	protected isContainer() {
 		return true;
+	}
+
+	protected getTagNameByChildNode(): string {
+		let tag: string = 'ul';
+		let existListItem: boolean = false;
+		let existButtonOrLink: boolean = false;
+		this.children.forEach((child, index) => {
+			if (child) {
+				const childNode = child as VNode;
+				const childName = childNode.properties.widget.widgetName;
+
+				if (childName === 'Link' || childName === 'Button') {
+					tag = 'div';
+					existButtonOrLink = true;
+					childNode.properties.widget.properties.isListItem = true;
+				}
+				if (childName === 'ListItem') {
+					existListItem = true;
+				}
+			}
+		});
+
+		if (existButtonOrLink && existListItem) {
+			console.error('ListItem and Button/Link can not be allowed at the same time in the ListGroup widget');
+		}
+
+		return tag;
 	}
 }
